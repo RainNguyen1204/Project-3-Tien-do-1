1f4502f743772b3c306a8962b5b6acc2bfa4d994
RainNguyen1204 <nguyencu1204005@gmail.com>
2024-08-29 19:36:46 +0700

    first commit

---
+++ f407 main 2/MDK-ARM/RTE/_f407_main_2/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'f407 main 2' 
+ * Target:  'f407 main 2' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
